f2e1ded50dda0266760b883d41f0b0c3d47e7489
Muhammad Khuzaimi <mkzbrs@mkzbrs-Mac.local>
2026-05-12 05:36:51 +0800

    Feat: Install Riverpod and Minio SDK, wrap app in ProviderScope

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Cloud Storage'),
+          child: Text(
+            'Cloud Storage\n(Powered by Riverpod & Minio)',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
